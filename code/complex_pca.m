9dac5474f81778a41253b42a68af6972157f210c
Sam Norman-Haignere <snormanhaignere@gmail.com>
2017-05-24 16:40:02 +0200

    complex_pca.m
    Illustrates the properties of applying PCA to complex-valued data

---
+++ code/complex_pca.m
@@ -0,0 +1,29 @@
+% Illustrates complex PCA
+% 
+% 2017-05-24: Created, Sam NH
+
+% complex stimulus
+X = randn(3,5) + sqrt(-1)*randn(3,5);
+
+% covariance
+C = X*transpose(conj(X));
+
+% eigen values of covariance matrix
+[E, D] = eig(C);
+
+% are the same as those computed by the svd up to a phase shift
+[U, S, V] = svd(X, 'econ');
+mag = abs(E' * U)
+ph = angle(E' * U)
+
+% reconstruct
+Xh = U*S*V';
+abs(X - Xh)
+
+% projection onto U
+SV = U' * X;
+SV - S*V'
+
+% projection onto V
+US = X * V;
+US - U*S
